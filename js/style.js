10ae0dc60a8d3427d6d6b6afd7255b0f46340606
RustyRadio <danielc@my.ccsu.edu>
2013-11-05 11:58:49 -0500

    Adding some jQuery

--- js/style.js
+++ js/style.js
@@ -1,22 +1,12 @@
 //= require vendor/jquery
 
-  function swapImages(){
-      var $active = $('#myGallery .active');
-      var $next = ($('#myGallery .active').next().length > 0) ? $('#myGallery .active').next() : $('#myGallery img:first');
-      $active.fadeOut(1000, function(){
-      $active.removeClass('active');
-      $next.fadeIn(1000).addClass('active');
-      });
-    }
-
   $(document).ready(function(){
-      setInterval('swapImages()', 2500);
-      $(".page-1").click(function() {
-        $('#second').removeClass('gallery');
-        $('#first').addClass('gallery');
+      $("#standard").click(function() {
+        var $active = $('.md-modal');
+        $active.addClass('.md-show');
       });
-      $(".page-2").click(function() {
-        $('#second').addClass('gallery');
-        $('#first').removeClass('gallery');
+      $('.md-cose').click(function() {
+        var $active = $('.md-modal');
+        $active.removeClass('.md-show');
       });
     });
