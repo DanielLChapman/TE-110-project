a018f9da36cefb91217a263bb3f8aaf17935cff6
RustyRadio <danielc@my.ccsu.edu>
2013-11-06 16:12:42 -0500

    Updates

---
+++ style.js
@@ -0,0 +1,9 @@
+      function modalWindow() {
+        var $active = $('.md-modal');
+        $active.addClass('.md-show');
+      };
+
+      function close() {
+        var $active = $('.md-modal');
+        $active.removeClass('.md-show');
+      };
